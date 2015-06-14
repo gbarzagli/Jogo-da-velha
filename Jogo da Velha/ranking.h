bdf369bf78af86d9b80949f85670d248815a6188
Gabriel Viseli Barzagli <gabriel.barzagli@hotmail.com>
2015-06-14 11:09:13 -0300

    ranking header update

--- Jogo da Velha/ranking.h
+++ Jogo da Velha/ranking.h
@@ -1,1 +1,8 @@
+// Exibe o ranking multiplayer
 void showMultiplayerRanking();
+// Exibe o ranking singleplayer
+void showSingleplayerRanking();
+// Adiciona um registro ao ranking multiplayer
+void addMultiplayerRanking(struct multiplayerResult result);
+// Adiciona um registro ao ranking singleplayer
+void addSingleplayerRanking(struct singleplayerResult result);
